ac48d244a9be480c1eb52bc57450ebae9a44129e
Greg Domorski <greg.domorski@gmail.com>
2015-05-12 17:36:07 -0400

    Update ABcheck.js

--- Coderbyte/ABcheck.js
+++ Coderbyte/ABcheck.js
@@ -1,7 +1,7 @@
 //BEST PRACTICE
 
 function ABCheck(str) { 
-  return /a...b/g.test(str);   
+  return /a...b/g.test(str);   //using a regular expression could easily solve this problem
 }
    
    
@@ -11,11 +11,11 @@ function ABCheck(str) {
 
 
 function ABCheck(str) { 
-	str === str.toLowerCase();
+	str === str.toLowerCase();  //make the whole string lowercase, this will not cause problems with upper case letters
   
-  for(var i = 0; i < str.length; i++){
+  for(var i = 0; i < str.length; i++){  //loop through the string
  
-     if((str[i] === "a" && str[i + 4] === "b") || (str[i] === "b" && str[i + 4] === "a")){
+     if((str[i] === "a" && str[i + 4] === "b") || (str[i] === "b" && str[i + 4] === "a")){ //see if the letters are apart from each other
   
        return true;
        }
