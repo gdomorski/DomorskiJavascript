a45f02ea4ff389bd295f423c0a4892266358f9b6
Greg Domorski <greg.domorski@gmail.com>
2015-06-08 21:44:31 -0400

    Update and rename brokenchatnotes.js to MakeBinaryTree.js

---
+++ Other Javascript Practice/MakeBinaryTree.js
@@ -0,0 +1,82 @@
+function MakeBinaryTree(val) {
+  
+  //var this = {};
+  this.value = val;
+  this.left = null;
+  this.right = null;
+  //return this;
+  
+}
+
+
+// this.value = 10
+// this.right = {this.value = 11, this.right = null, this.left};
+// this.left = null;
+
+
+MakeBinaryTree.prototype.add = function(val) {
+  
+  
+  function subRoutine(node) {
+    //value, right, left
+    if(val > node.value){
+      if(node.left){
+        subRoutine(node.left);
+      }else{
+        node.left = new MakeBinaryTree(val)
+      }
+      //do something this
+      
+    } else if (val < node.value) {
+      if(node.right){
+        subRoutine(node.right)
+      } else {
+        node.right = new MakeBinaryTree(val);
+      }
+      
+    }
+    
+  }
+  // what do we pass in?
+  subRoutine(this);
+  
+  
+};
+
+
+MakeBinaryTree.prototype.contains = function(val) {
+  
+  //create a boolean value and set it to false as a default
+  var comparison = false;
+
+  function subRoutine(node){
+
+  	//if they are equal
+  	//return true
+  	if(val == node.value){
+  		return comparison = true;
+  	}
+
+
+  	//if it is less, check the left side, recursively
+  	if(val > node.value){
+  		if(node.left){
+  		subRoutine(node.left);
+  		}
+  	}	
+
+  	//if it is greater check the right, recursively
+  	else if (val < node.value){
+  		if(node.right){
+  		subRoutine(node.right);
+  	    }
+    }
+}
+
+  subRoutine(this);
+
+
+  		//return the boolean value;
+  		return comparison;
+};
+
