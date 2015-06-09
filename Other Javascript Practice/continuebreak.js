8dc63a44fcf7118a16ea73c1b3eae6a0fb534196
Greg Domorski <greg.domorski@gmail.com>
2015-06-09 03:49:26 -0400

    Update continuebreak.js

--- Other Javascript Practice/continuebreak.js
+++ Other Javascript Practice/continuebreak.js
@@ -1,15 +1,16 @@
 //Notes on continue and break statements
 
-
-for(var i = 0; i < 10; i++){
-	if(i === 4){
-		break;
-	}
-	console.log("The current value is " + i)
-
+  for(var i = 0; i < 10; i++){
+    if(i === 4){
+      break;  //break jumps of ouf the loop
+    }
 }
+	console.log(i);
 
-console.log("This is the end");
 
-
+ for(var i = 1; i < 11; i++){
+    if(i === 4){
+      continue;  //continue jumps over the current value
+    }
+    console.log(i); //will log every number but 4
 }
