e4302a969be3d0a77b5667defab90a10f8b7076c
Greg Domorski <greg.domorski@gmail.com>
2015-06-14 23:00:01 -0400

    Update wordCount.js

--- Coderbyte/wordCount.js
+++ Coderbyte/wordCount.js
@@ -1,8 +1,6 @@
 function WordCount(str) {
-
-
-	str = str.split(" ");			// Turn the string into an array of words by passing a single space into the .split method...
-
-    return str.length;    // return the length of the new array as the answer
-
+// Turn the string into an array of words by passing a single space into the .split method...
+str = str.split(" ");
+// return the length of the new array as the answer
+return str.length;   
 }
