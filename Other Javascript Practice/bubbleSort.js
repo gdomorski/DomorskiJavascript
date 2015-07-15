04f5e4bc8625f84a6c2da09ab73c87ef12039eab
Greg Domorski <greg.domorski@gmail.com>
2015-07-15 00:38:23 -0400

    Update bubbleSort.js

--- Other Javascript Practice/bubbleSort.js
+++ Other Javascript Practice/bubbleSort.js
@@ -1,21 +1,33 @@
+function bubblesort(array){
+	//if it is not an array or the the array's length is less than one
+	if(!Array.isArray(array) || array.length < 1){
+		//throw new error();
+		throw new Error();
+	}
 
+	//create a new variable called changed
+	var changed;
 
-var bubbleSort = function(arr) {
-  arr.forEach(function (value, index){
-    for(var j = index + 1; j < arr.length; j++){
-      if(value > arr[j]){
-        var temp = arr[index];
-        arr[index] = arr[j];
-        arr[j] = temp;
-      }
-    }
-  });
-  return arr;
-};
+	//iterate through the array, with the length set to minus one because the last index has already been compared with what is in front of i
+	for(var i = 0; i < array.length - 1; i++){
+		//set changed to false;
+		changed = false;
+		//make a inner loop that loops through the array
+		for(var k = 0; k < array.length - 1; k++){
+			if(array[k] > array[k + 1]){
+				//set changed to true;
+				changed = true;
+				//set the variable to a temp value;
+				var temp = array[k];
+				//set the value to the one in front of it
+				array[k] = array[k+1];
+				//set the value in front to the larger value;
+				array[k + 1] = temp;
+			}
+		}
+	}
 
+	//return the array;
+	return array;
 
-
-
-
-
-Bubblesort([1,2,4,12,11,9]);
+}
