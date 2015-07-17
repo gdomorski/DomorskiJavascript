d1c5f6f53f6f34257b0638bc3068879cc5729fa6
Greg Domorski <greg.domorski@gmail.com>
2015-07-17 11:14:47 -0400

    Update bubbleSort.js

--- Other Javascript Practice/bubbleSort.js
+++ Other Javascript Practice/bubbleSort.js
@@ -5,18 +5,14 @@ function bubblesort(array){
 		throw new Error();
 	}
 
-	//create a new variable called changed
-	var changed;
 
 	//iterate through the array, with the length set to minus one because the last index has already been compared with what is in front of i
 	for(var i = 0; i < array.length - 1; i++){
-		//set changed to false;
-		changed = false;
+
 		//make a inner loop that loops through the array
 		for(var k = 0; k < array.length - 1; k++){
 			if(array[k] > array[k + 1]){
-				//set changed to true;
-				changed = true;
+
 				//set the variable to a temp value;
 				var temp = array[k];
 				//set the smaller value to the left
